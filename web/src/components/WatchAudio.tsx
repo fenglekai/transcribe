4290824d06d243a9c55582a282d14f117419ab11
fenglekai <674640130@qq.com>
2024-11-22 17:06:05 +0800

    refactor: 💡 封装函数WatchChromeTag

--- web/src/components/WatchAudio.tsx
+++ web/src/components/WatchAudio.tsx
@@ -1,14 +1,12 @@
 import { useState, useCallback, useEffect } from "react";
 import { Button, Col, Row, Input } from "antd";
 import { fetchSpeedToText } from "../sdk/service";
-import { getWaveBlob } from "webm-to-wav-converter";
+import { WatchChromeTag } from "../sdk/AudioRTC";
 
 const { TextArea } = Input;
 
-let mediaStream: MediaStream | undefined;
-let mediaRecorder: MediaRecorder | undefined;
-const recordedChunks: Blob[] = [];
-let streamInterval: string | number | NodeJS.Timeout | undefined;
+const watchChromeTag = new WatchChromeTag();
+
 let fetchFrame: number;
 let text = "";
 let translation = "";
@@ -24,39 +22,41 @@ export default function WatchAudio() {
     translation = "";
     addRowStatus = true;
     setWatch(true);
-    handleSelectDisplayMedia();
+    try {
+      await watchChromeTag.selectDisplayMedia((sound) => {
+        if (!sound) {
+          if (!addRowStatus) {
+            text += "\n\n";
+            translation += "\n\n";
+            addRowStatus = true;
+          }
+          return console.log("not sound");
+        }
+      });
+    } catch (error) {
+      handleStop()
+    }
   };
 
   const handleStop = () => {
-    clearInterval(streamInterval);
     cancelAnimationFrame(fetchFrame);
-    if (mediaStream) {
-      mediaStream.getTracks().forEach((track) => {
-        track.stop();
-      });
-    }
-    if (mediaRecorder) {
-      if (mediaRecorder.state === "recording") {
-        mediaRecorder.stop();
-      }
-    }
-    recordedChunks.length = 0;
     setWatch(false);
+    watchChromeTag.stopWatch()
   };
 
   // 获取转换文本
   const fetchTranscribe = useCallback(async () => {
     try {
-      if (recordedChunks.length > 0) {
-        const wavBlob = recordedChunks.shift();
+      if (watchChromeTag.getChunksLength() > 0) {
+        const wavBlob = watchChromeTag.getRecordedChunks();
         const res = await fetchSpeedToText(wavBlob as Blob);
         const { text: t, translation: tlt } = res.data;
         if (watch) {
           text += t;
           translation += tlt;
           addRowStatus = false;
-          setTextRender(text)
-          setTranslationRender(translation)
+          setTextRender(text);
+          setTranslationRender(translation);
         }
       }
       fetchFrame = requestAnimationFrame(fetchTranscribe);
@@ -70,95 +70,6 @@ export default function WatchAudio() {
       fetchFrame = requestAnimationFrame(fetchTranscribe);
     }
   }, [watch, fetchTranscribe]);
-
-  // 触发显示媒体选择
-  const handleSelectDisplayMedia = async () => {
-    try {
-      mediaStream = await navigator.mediaDevices.getDisplayMedia({
-        audio: { echoCancellation: true },
-        video: {
-          width: 320,
-          height: 240,
-          frameRate: 30,
-        },
-      });
-      mediaRecorder = new MediaRecorder(mediaStream, {
-        mimeType: "video/webm; codecs=pcm",
-      });
-      // 数据可用（录屏结束）时的回调
-      mediaRecorder.ondataavailable = async (event) => {
-        // 转换webm -> wav
-        const wavBlob = await getWaveBlob(event.data, false);
-        await hasSound(wavBlob, (sound) => {
-          if (!sound) {
-            if (!addRowStatus) {
-              text += "\n\n";
-              translation += "\n\n";
-              addRowStatus = true;
-            }
-            return console.log("not sound");
-          }
-          console.log("has sound");
-          if (wavBlob.size > 0) {
-            recordedChunks.push(wavBlob);
-          }
-        });
-      };
-      mediaRecorder.start();
-      // 设置定时器，每3秒发送一次数据
-      const sendToServer = async () => {
-        try {
-          if (mediaRecorder) {
-            mediaRecorder.stop();
-            mediaRecorder.start();
-          }
-        } catch (error) {
-          handleStop();
-        }
-      };
-      streamInterval = setInterval(sendToServer, 3000);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // 分析声音波形
-  const hasSound = async (
-    blob: Blob,
-    callback: (hasSoundFlag: boolean) => void
-  ) => {
-    let hasSoundFlag = false;
-    const audioContext = new AudioContext();
-    // 将 Blob 转换为 AudioBuffer
-    const reader = new FileReader();
-    reader.onload = async (event) => {
-      if (!event.target) return;
-      if (!(event.target.result instanceof ArrayBuffer)) return;
-      try {
-        const audioBuffer = await audioContext.decodeAudioData(
-          event.target.result
-        );
-        // 获取音频数据
-        const channelData = audioBuffer.getChannelData(0);
-
-        // 判断音频是否有波动
-        const threshold = 0.01;
-        for (let i = 0; i < channelData.length; i++) {
-          if (Math.abs(channelData[i]) > threshold) {
-            hasSoundFlag = true;
-            break;
-          }
-        }
-        callback(hasSoundFlag);
-      } catch (error) {
-        console.error("音频解码失败:", error);
-      }
-    };
-    reader.onerror = (error) => {
-      console.error("文件读取失败:", error);
-    };
-    reader.readAsArrayBuffer(blob);
-  };
 
   return (
     <>
--- web/src/sdk/AudioRTC.ts
+++ web/src/sdk/AudioRTC.ts
@@ -1,51 +1,180 @@
-import RecordRTC from 'recordrtc';
-import { getWaveBlob } from 'webm-to-wav-converter'
+import RecordRTC from "recordrtc";
+import { getWaveBlob } from "webm-to-wav-converter";
+
+// 分析声音波形
+const hasSound = async (
+  blob: Blob,
+  callback: (hasSoundFlag: boolean) => void
+) => {
+  let hasSoundFlag = false;
+  const audioContext = new AudioContext();
+  // 将 Blob 转换为 AudioBuffer
+  const reader = new FileReader();
+  reader.onload = async (event) => {
+    if (!event.target) return;
+    if (!(event.target.result instanceof ArrayBuffer)) return;
+    try {
+      const audioBuffer = await audioContext.decodeAudioData(
+        event.target.result
+      );
+      // 获取音频数据
+      const channelData = audioBuffer.getChannelData(0);
+
+      // 判断音频是否有波动
+      const threshold = 0.01;
+      for (let i = 0; i < channelData.length; i++) {
+        if (Math.abs(channelData[i]) > threshold) {
+          hasSoundFlag = true;
+          break;
+        }
+      }
+      callback(hasSoundFlag);
+    } catch (error) {
+      console.error("音频解码失败:", error);
+    }
+  };
+  reader.onerror = (error) => {
+    console.error("文件读取失败:", error);
+  };
+  reader.readAsArrayBuffer(blob);
+};
 
 export default class AudioRTC {
-    stream!: MediaStream;
+  stream!: MediaStream;
 
-    recorder!: RecordRTC
+  recorder!: RecordRTC;
 
-    /**
-     * 开始录制
-     */
-    async startRecording() {
-        if (!this.recorder) {
-            this.stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+  /**
+   * 开始录制
+   */
+  async startRecording() {
+    if (!this.recorder) {
+      this.stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-            this.recorder = new RecordRTC(this.stream, {
-                type: 'audio'
-            })
+      this.recorder = new RecordRTC(this.stream, {
+        type: "audio",
+      });
+    }
+
+    this.recorder.startRecording();
+  }
+
+  /**
+   * 结束录制
+   * @returns
+   */
+  stopRecording(): Promise<Blob> {
+    if (!this.recorder) {
+      return Promise.reject("Recorder is not initialized");
+    }
+    this.stream.getTracks().forEach((track) => track.stop());
+    return new Promise((resolve) => {
+      this.recorder.stopRecording(() => {
+        const blob = this.recorder.getBlob();
+
+        resolve(blob);
+      });
+    });
+  }
+
+  /**
+   * 获取 blob
+   * @returns
+   */
+  getWaveBlob() {
+    const blob = this.recorder.getBlob();
+
+    return getWaveBlob(blob, false);
+  }
+}
+
+export class WatchChromeTag {
+  mediaStream!: MediaStream;
+  mediaRecorder!: MediaRecorder;
+  streamInterval!: string | number | NodeJS.Timeout;
+  recordedChunks: Blob[];
+
+  constructor() {
+    this.recordedChunks = [];
+  }
+
+  /**
+   * 获取音频队列长度
+   * @returns recordedChunks数组长度
+   */
+  getChunksLength() {
+    return this.recordedChunks.length;
+  }
+
+  /**
+   * 获取音频块
+   * @returns wavBlob
+   */
+  getRecordedChunks() {
+    if (this.recordedChunks.length > 0) {
+      return this.recordedChunks.shift();
+    }
+    throw Error("队列中没有音频");
+  }
+
+  /**
+   * 触发显示媒体选择
+   * @param callback 调用返回，sound是否有声音，wavBlob音频流
+   */
+  async selectDisplayMedia(callback: (sound: boolean, wavBlob: Blob) => void) {
+    this.mediaStream = await navigator.mediaDevices.getDisplayMedia({
+      audio: { echoCancellation: true },
+      video: {
+        width: 320,
+        height: 240,
+        frameRate: 30,
+      },
+    });
+    this.mediaRecorder = new MediaRecorder(this.mediaStream, {
+      mimeType: "video/webm; codecs=pcm",
+    });
+    // 数据可用（录屏结束）时的回调
+    this.mediaRecorder.ondataavailable = async (event) => {
+      // 转换webm -> wav
+      const wavBlob = await getWaveBlob(event.data, false);
+      await hasSound(wavBlob, (sound) => {
+        callback(sound, wavBlob);
+        if (sound && wavBlob.size > 0) {
+          console.log("has sound");
+          this.recordedChunks.push(wavBlob);
         }
-
-        this.recorder.startRecording()
-    }
-
-    /**
-     * 结束录制
-     * @returns 
-     */
-    stopRecording(): Promise<Blob> {
-        if (!this.recorder) {
-            return Promise.reject('Recorder is not initialized')
+      });
+    };
+    this.mediaRecorder.start();
+    // 设置定时器，每3秒发送一次数据
+    const sendToServer = async () => {
+      try {
+        if (this.mediaRecorder) {
+          this.mediaRecorder.stop();
+          this.mediaRecorder.start();
         }
-        this.stream.getTracks().forEach(track => track.stop())
-        return new Promise((resolve) => {
-            this.recorder.stopRecording(() => {
-                const blob = this.recorder.getBlob()
+      } catch (error) {
+        this.stopWatch();
+      }
+    };
+    this.streamInterval = setInterval(sendToServer, 3000);
+  }
 
-                resolve(blob)
-            })
-        })
+  /**
+   * 停止监听
+   */
+  stopWatch() {
+    clearInterval(this.streamInterval);
+    if (this.mediaStream) {
+      this.mediaStream.getTracks().forEach((track) => {
+        track.stop();
+      });
     }
-
-    /**
-     * 获取 blob
-     * @returns 
-     */
-    getWaveBlob() {
-        const blob = this.recorder.getBlob()
-
-        return getWaveBlob(blob, false);
+    if (this.mediaRecorder) {
+      if (this.mediaRecorder.state === "recording") {
+        this.mediaRecorder.stop();
+      }
     }
+    this.recordedChunks.length = 0;
+  }
 }
