82d5c7e5f75f8a15e06662d202334090f71ae269
fenglekai <674640130@qq.com>
2024-12-03 14:41:30 +0800

    feat: 🎸 更新选择设备联动监听

--- python/flet_main.py
+++ python/flet_main.py
@@ -2,27 +2,28 @@ import time
 import flet as ft
 import numpy as np
 import sounddevice as sd
-import threading
 from sound import SoundTranscribe
 
 
 class FletMain:
     def __init__(self):
         self.page = None
-        self.devices = [
-            d
-            for d in sd.query_devices()
-            if d["max_output_channels"] > 0 or d["max_input_channels"] > 0
-        ]
-        self.device_detail = ft.Text()
-        self.select_device = sd.default.device[0]
+        self.button_status = False
+        self.sound_transcribe = SoundTranscribe(
+            self.set_volume,
+            self.add_text,
+            self.add_row,
+            self.add_translation,
+        )
+        self.devices = self.sound_transcribe.get_devices()
         self.device_list = ft.Dropdown(
             label="选择设备",
             options=[
                 ft.dropdown.Option(index, d["name"])
                 for index, d in enumerate(self.devices)
             ],
-            on_change=self.on_select_device,
+            on_change=self.handle_select_device,
+            disabled=self.button_status,
         )
         self.volume_bar = ft.ProgressBar(width=200, value=0)
         self.device_select = ft.Row(
@@ -41,16 +42,8 @@ class FletMain:
                             "刷新设备",
                         ),
                         opacity_on_click=0.3,
-                        on_click=lambda e: self.refresh_device(e),
+                        on_click=self.refresh_device,
                     ),
-                    margin=10,
-                    padding=10,
-                    alignment=ft.alignment.center,
-                    height=100,
-                    border_radius=10,
-                ),
-                ft.Container(
-                    content=self.device_detail,
                     margin=10,
                     padding=10,
                     alignment=ft.alignment.center,
@@ -67,10 +60,9 @@ class FletMain:
                 ),
             ]
         )
-        self.stop_event = threading.Event()
         self.text_count = ""
         self.translation_text_count = ""
-        self.button_status = False
+
         self.list_view = ft.ListView(
             expand=1,
             spacing=10,
@@ -108,81 +100,73 @@ class FletMain:
         self.row = ft.Row(
             controls=[self.sound_container, self.translation_container],
         )
-        self.sound_transcribe = SoundTranscribe(
-            self.add_text, self.add_row, self.add_translation
-        )
-
+        # self.on_load()
         ft.app(self.flet_page)
 
     def flet_page(self, page: ft.Page):
+        """Flet Page"""
         self.page = page
-        # self.start_audio_stream()
         page.title = "Less write realtime speech recognition"
         page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
         page.auto_scroll = True
         page.scroll = ft.ScrollMode.HIDDEN
 
         # 窗口关闭事件
-        def handle_window_event(e):
-            if e.data == "close":
-                # 用户尝试关闭窗口，弹出确认对话框
-                print("close")
-                self.sound_transcribe.close_sound()
-                page.window.destroy()
-
         page.window.prevent_close = True
-        page.window.on_event = handle_window_event
+        page.window.on_event = self.handle_window_event
 
         page.floating_action_button = ft.FloatingActionButton(
             icon=ft.icons.PLAY_ARROW,
-            on_click=self.fab_pressed,
+            on_click=self.handle_play,
             bgcolor=ft.colors.CYAN_300,
         )
         page.add(self.device_select)
         page.add(self.row)
 
-    def start_audio_stream(self):
-        def audio_callback(indata, frames, time, status):
-            volume_norm = np.linalg.norm(indata)
+    def on_load(self):
+        """页面加载完成后执行的逻辑"""
+        print("on_load")
+        print(sd.default.device[0])
+        device_info = self.devices[sd.default.device[0]]
+        self.sound_transcribe.set_stream(device_info["index"])
+
+    def handle_window_event(self, e):
+        if e.data == "close":
+            # 用户尝试关闭窗口，弹出确认对话框
+            print("close")
+            self.sound_transcribe.close_sound()
+            self.page.window.destroy()
+
+    def set_volume(self, in_data, percent=1):
+        """设置音量条"""
+        try:
+            volume_norm = np.linalg.norm(in_data) * percent
             self.volume_bar.value = min(1.0, volume_norm)
-            self.page.update()
+            self.volume_bar.update()
+        except Exception as e:
+            pass
 
-        def audio_thread():
-            with sd.InputStream(
-                device=self.select_device,
-                channels=1,
-                callback=audio_callback,
-                blocksize=1024,
-                samplerate=16000,
-            ):
-                while not self.stop_event.is_set():
-                    sd.sleep(100)
-
-        self.stop_event.clear()
-        threading.Thread(target=audio_thread, daemon=True).start()
-
-    def stop_audio_stream(self):
-        sd.stop()
-        self.stop_event.set()
-
-    def on_select_device(self, e):
+    def handle_select_device(self, e):
+        """选择设备事件"""
+        self.sound_transcribe.close_sound()
         index = int(self.device_list.value)
-        selected_device = self.device_list.options[index]
         device_info = self.devices[index]
-        self.select_device = device_info["index"]
-        self.device_detail.value = (
-            f"Selected Device: {selected_device.text}\n"
-            f"Max Input Channels: {device_info['max_input_channels']}\n"
-            f"Max Output Channels: {device_info['max_output_channels']}\n"
-            f"Sample Rate: {device_info['default_samplerate']}"
-        )
+        self.sound_transcribe.set_stream(device_info["index"])
         self.volume_bar.value = 0
         self.page.update()
-        self.stop_audio_stream()
         time.sleep(1)
-        self.start_audio_stream()
 
-    def fab_pressed(self, e):
+    def refresh_device(self, e):
+        """刷新设备事件"""
+        self.devices = self.sound_transcribe.get_devices()
+        self.device_list.options = [
+            ft.dropdown.Option(index, d["name"]) for index, d in enumerate(self.devices)
+        ]
+        self.device_list.update()
+        time.sleep(1)
+
+    def handle_play(self, e):
+        """触发监听事件"""
         if self.button_status == False:
             print("start")
             self.button_status = True
@@ -196,26 +180,14 @@ class FletMain:
             self.page.floating_action_button.update()
             self.sound_transcribe.close_sound()
 
-    def refresh_device(self, e):
-        self.devices = [
-            d
-            for d in sd.query_devices()
-            if d["max_output_channels"] > 0 or d["max_input_channels"] > 0
-        ]
-        self.device_list.options = [
-            ft.dropdown.Option(index, d["name"]) for index, d in enumerate(self.devices)
-        ]
-        self.device_list.update()
-        self.stop_audio_stream()
-        time.sleep(1)
-        self.start_audio_stream()
-
     def add_text(self, text):
+        """触发音频转译添加文本"""
         self.text_count += text
         self.sound_text.value = self.text_count
         self.sound_text.update()
 
     def add_row(self):
+        """触发未监听到音频添加换行"""
         self.text_count = ""
         self.sound_text = ft.Text(selectable=True)
         self.list_view.controls.append(self.sound_text)
@@ -225,6 +197,7 @@ class FletMain:
         self.page.update()
 
     def add_translation(self, text):
+        """触发翻译"""
         self.translation_text_count += text
         self.translation_text.value = self.translation_text_count
         self.translation_text.update()
--- python/sound.py
+++ python/sound.py
@@ -11,18 +11,28 @@ import requests
 class SoundTranscribe:
     def __init__(
         self,
-        sendText: Callable[[str], None],
-        addRow: Callable[[], None],
-        sendTranslation: Callable[[str], None],
+        sound_callback: Callable[[any], None] | None,
+        send_text: Callable[[str], None] | None,
+        add_row: Callable[[], None] | None,
+        send_translation: Callable[[str], None] | None,
     ):
-        self.sendText = sendText  # 发送文本
-        self.addRow = addRow  # 新增行
-        self.sendTranslation = sendTranslation  # 新增行
+        """声音转录
+        sound_callback: input stream的返回数据in_data
+        send_text: 转译的文本
+        add_row: 未识别到声音换行
+        send_translation: 翻译文本
+        """
+        self.sound_callback = sound_callback
+        self.send_text = send_text
+        self.add_row = add_row
+        self.send_translation = send_translation
         self.no_sound = 0  # 无声次数
         self.running = False  # 是否运行
         self.current_devices = None  # 当前使用设备
         self.transcribe_thread = None  # 音频转文本线程
         self.stop_event = threading.Event()
+        self.devices = sd.query_devices()
+
         # 参数配置
         self.RATE = 16000  # 采样率
         self.BLOCK_SIZE = 1024  # 分块大小
@@ -31,8 +41,16 @@ class SoundTranscribe:
         self.D_TYPE = np.float32  # numpy.dtype
         self.VOLUME_THRESHOLD = 0.005  # 音量阈值（根据需要调整）
         self.audio_queue = queue.Queue()  # 源音频队列
+        self.stream = sd.InputStream(
+            callback=self.audio_callback,
+            channels=self.CHANNELS,
+            samplerate=self.RATE,
+            blocksize=self.BLOCK_SIZE,
+            dtype=self.D_TYPE,
+        )
 
     def monitor_devices(self):
+        """监控设备信息"""
         devices = sd.query_devices()
         for i, device in enumerate(devices):
             print(f"Index: {i}, Name: {device['name']}, Type: {device['hostapi']}")
@@ -41,61 +59,80 @@ class SoundTranscribe:
         print(f"Default input device: {default_input_device}")
         print(f"Default output device: {default_output_device}")
 
+    def get_devices(self):
+        """获取设备"""
+        self.devices = sd.query_devices()
+        return self.devices
+
+    def set_stream(self, device):
+        """设置流媒体"""
+        self.close_sound()
+        self.stream = sd.InputStream(
+            device=device,
+            callback=self.audio_callback,
+            channels=self.CHANNELS,
+            samplerate=self.RATE,
+            blocksize=self.BLOCK_SIZE,
+            dtype=self.D_TYPE,
+        )
+        with self.stream:
+            while True:
+                sd.sleep(int(self.BLOCK_DURATION * 1000))
+
     def compute_rms(self, audio_data):
         """计算音频数据的均方根值（RMS）"""
         return np.sqrt(np.mean(np.square(audio_data)))
 
-    # 音频流回调函数
     def audio_callback(self, indata, frames, time, status):
+        """音频流回调函数"""
         self.audio_queue.put(indata.copy())  # 放入队列
+        self.sound_callback(indata)
 
-    # 音频转录函数
     def transcribe_audio(
         self,
     ):
-            while self.running:
-                audio_data = []
-                for _ in range(int(self.RATE * self.BLOCK_DURATION / self.BLOCK_SIZE)):
-                    audio_data.append(self.audio_queue.get())
-                audio_data = (
-                    np.concatenate(audio_data, axis=0).flatten().astype(self.D_TYPE)
-                )
+        """音频转录函数"""
+        while self.running:
+            audio_data = []
+            for _ in range(int(self.RATE * self.BLOCK_DURATION / self.BLOCK_SIZE)):
+                audio_data.append(self.audio_queue.get())
+            audio_data = (
+                np.concatenate(audio_data, axis=0).flatten().astype(self.D_TYPE)
+            )
 
-                # 检测音量，如果低于阈值则跳过转录
-                rms_value = self.compute_rms(audio_data)
-                # if rms_value < self.VOLUME_THRESHOLD:
-                #     print("Sound is too low, skipping transcription.")
-                #     if self.no_sound < 1:
-                #         self.addRow()
-                #         self.no_sound += 1
-                #     continue  # 声音小，跳过此段音频的转录
+            # 检测音量，如果低于阈值则跳过转录
+            # rms_value = self.compute_rms(audio_data)
+            # if rms_value < self.VOLUME_THRESHOLD:
+            #     print("Sound is too low, skipping transcription.")
+            #     if self.no_sound < 1:
+            #         self.add_row()
+            #         self.no_sound += 1
+            #     continue  # 声音小，跳过此段音频的转录
 
-                try:
-                    data_bytes = audio_data.tobytes()
-                    files = {
-                        "audio": ("audio.wav", data_bytes, "application/octet-stream"),
-                        "timestamp": (None, str(time.time())),
-                    }
-                    url = "http://localhost:9090/python/soundDevice"
-                    response = requests.post(
-                        url,
-                        files=files
-                    )
-                    json_data = response.json()
-                    text = json_data['text']
-                    translation = json_data['translation']
-                    if text != "" and text != "嗯":
-                        self.sendText(text)
-                        self.sendTranslation(translation)
-                        self.no_sound = 0
-                    else:
-                        if self.no_sound < 1:
-                            self.addRow()
-                            self.no_sound += 1
-                except Exception as e:
-                    print(e)
+            try:
+                data_bytes = audio_data.tobytes()
+                files = {
+                    "audio": ("audio.wav", data_bytes, "application/octet-stream"),
+                    "timestamp": (None, str(time.time())),
+                }
+                url = "http://localhost:9090/python/soundDevice"
+                response = requests.post(url, files=files)
+                json_data = response.json()
+                text = json_data["text"]
+                translation = json_data["translation"]
+                if text != "" and text != "嗯":
+                    self.send_text(text)
+                    self.send_translation(translation)
+                    self.no_sound = 0
+                else:
+                    if self.no_sound < 1:
+                        self.add_row()
+                        self.no_sound += 1
+            except Exception as e:
+                print(e)
 
     def sound_transcribe(self):
+        """开始转译"""
         self.running = True
         # 创建转录线程并监听
         self.stop_event.clear()
@@ -104,26 +141,20 @@ class SoundTranscribe:
         self.transcribe_thread.start()
 
         # 监听系统音频（通过虚拟音频设备作为输入）
-        stream = sd.InputStream(
-            callback=self.audio_callback,
-            channels=self.CHANNELS,
-            samplerate=self.RATE,
-            blocksize=self.BLOCK_SIZE,
-            dtype=self.D_TYPE,
-        )
-        with stream:
-            try:
+        try:
+            with self.stream:
                 while self.running:
                     sd.sleep(int(self.BLOCK_DURATION * 1000))
-            except KeyboardInterrupt:
-                # 关闭音频流
-                sd.stop()
-                sys.exit()
-                self.stop_event.set()
+        except KeyboardInterrupt:
+            # 关闭音频流
+            sd.stop()
+            sys.exit()
+            self.stop_event.set()
 
     def close_sound(self):
+        """关闭监听"""
         self.running = False
-        qsize = self.audio_queue.qsize()
+        self.stream.close()
         while not self.audio_queue.empty():
             self.audio_queue.get_nowait()
         sd.stop()
@@ -139,6 +170,6 @@ def callbackRow():
 
 
 if __name__ == "__main__":
-    sound = SoundTranscribe(callbackText, callbackRow)
+    sound = SoundTranscribe(callbackText, callbackText, callbackRow, callbackText)
     # sound.sound_transcribe()
     sound.monitor_devices()
